8c8e2eb9b7c7106aa9d5d340988934942a352ada
jbednarek <jbednarek@nugato.pl>
2017-11-07 00:57:04 +0100

    Integrate SyliusResourceBundle + Create simple page Resource

---
+++ src/Nugato/Bundle/NuCmsBundle/Entity/Page.php
@@ -0,0 +1,64 @@
+<?php
+
+/*
+ * This file is part of the NuCms package.
+ *
+ * (c) Jacek Bednarek
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+declare(strict_types=1);
+
+namespace Nugato\Bundle\NuCmsBundle\Entity;
+
+use Sylius\Component\Resource\Model\ResourceInterface;
+
+class Page implements ResourceInterface
+{
+    /**
+     * @var int
+     */
+    private $id;
+
+    /**
+     * @var string
+     */
+    private $title;
+
+    /**
+     * Get id
+     *
+     * @return int
+     */
+    public function getId()
+    {
+        return $this->id;
+    }
+
+    /**
+     * Set title
+     *
+     * @param string $title
+     *
+     * @return Page
+     */
+    public function setTitle(string $title)
+    {
+        $this->title = $title;
+
+        return $this;
+    }
+
+    /**
+     * Get title
+     *
+     * @return string
+     */
+    public function getTitle(): string
+    {
+        return $this->title;
+    }
+}
+
